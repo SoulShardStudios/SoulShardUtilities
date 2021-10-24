ecdbf00fbb87f099990164601c949936d545ab94
Benjamin Hines <75751722+Lubba-64@users.noreply.github.com>
2021-10-24 09:39:20 -0400

    added line renderer

--- Runtime/Vector/VectorMath.cs
+++ Runtime/Vector/VectorMath.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections.Generic;
 namespace SoulShard.Utils
 {
     public static class VectorMath
@@ -102,6 +103,15 @@ namespace SoulShard.Utils
                 return null;
             for (int i = 0; i < toBeTranslated.Length; i++)
                 toReturn[i] = toBeTranslated[i] + translateBy;
+            return toReturn;
+        }
+        public static Vector2Int[] TranslatePositionsToArray(Vector2Int[] points, Vector2Int[] shape)
+        {
+            HashSet<Vector2Int> hash = new HashSet<Vector2Int>();
+            foreach (Vector2Int v in points)
+                hash.UnionWith(TranslateVector2IntArray(shape, v));
+            Vector2Int[] toReturn = new Vector2Int[hash.Count];
+            hash.CopyTo(toReturn);
             return toReturn;
         }
         #endregion
