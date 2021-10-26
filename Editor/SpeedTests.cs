01b67e66fa8040ec545f6c1becf6620f2fa1519c
Benjamin Hines <75751722+Lubba-64@users.noreply.github.com>
2021-10-26 11:30:35 -0400

    added more, better speed tests

--- Editor/SpeedTests.cs
+++ Editor/SpeedTests.cs
@@ -4,19 +4,40 @@ namespace SoulShard.Utils
 {
     public static class SpeedTests
     {
-        public static (float, float) SpeedTest(Action a1, Action a2)
+        public const string Units = "In Miliseconds";
+        public static float GetDiffInMS(float a, float b) => Mathf.Abs(a - b) * 1000f;
+        public static float SpeedTest(Action a)
         {
             float _start = Time.realtimeSinceStartup;
-            a1?.Invoke();
-            float _switch = Time.realtimeSinceStartup;
-            a2?.Invoke();
+            a?.Invoke();
             float _end = Time.realtimeSinceStartup;
-            return ((_switch - _start) * 1000f, (_end - _switch) * 1000f);
+            return (_end- _start) * 1000f;
         }
-        public static void SpeedTestDebug(Action a1, Action a2)
+        public static void SpeedTestDebug(Action a, string taskName)
         {
-            (float, float) speedTestResults = SpeedTest(a1, a2);
-            Debug.Log(("In Miliseconds", "A", speedTestResults.Item1, "B", speedTestResults.Item2));
+            float speedTestResults = SpeedTest(a);
+            Debug.Log((taskName, Units, speedTestResults));
         }
+
+        public static float[] SpeedTestDebug(Action[] actions)
+        {
+            float[] diffs = new float[actions.Length];
+            for(int i = 0; i < actions.Length; i++)
+            {
+                float start = Time.realtimeSinceStartup;
+                actions[i]?.Invoke();
+                diffs[i] = GetDiffInMS(Time.realtimeSinceStartup, start);
+            }
+            return diffs;
+        }
+        public static void SpeedTestDebug(Action[] actions, string[] taskNames = null, string? testName = null)
+        {
+            float[] speedTestResults = SpeedTestDebug(actions);
+            Debug.Log(testName != null ? testName : "Test");
+            for(int i = 0; i < speedTestResults.Length; i++)
+                Debug.Log((taskNames?[i] ?? i.ToString(), Units, speedTestResults[i]));
+        }
+
+
     }
 }
