ac5aa62dea41147e884802fbd3375d666f21ba05
Benjamin Hines <75751722+Lubba-64@users.noreply.github.com>
2021-11-03 15:39:57 -0400

    added positive modulo function

---
+++ Runtime/Math/MathUtility.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+namespace SoulShard.Utils
+{
+    public class MathUtility : MonoBehaviour
+    {
+        public static uint PositiveMod(int value, uint m)
+        {
+            int mod = value % (int)m;
+            if (mod < 0)
+                mod += (int)m;
+            return (uint)mod;
+        }
+        public static int PositiveMod(int value, int m)
+        {
+            int mod = value % m;
+            if (mod < 0)
+                mod += m;
+            return mod;
+        }
+    }
+}
