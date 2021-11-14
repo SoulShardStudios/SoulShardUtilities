05d5e2dbf0800cfb8bc7cbd7ed1a8e27603d0930
Benjamin Hines <75751722+Lubba-64@users.noreply.github.com>
2021-11-14 16:48:05 -0500

    added a dynamic collision mesh

---
+++ Runtime/DynamicCollisionMesh.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using System.Collections.Generic;
+namespace SoulShard.Utils
+{
+    /// <summary>
+    /// this is an object that allows collision mesh data to be pushed to it and then rendered into a final mesh.
+    /// </summary>
+    public class DynamicCollisionMesh
+    {
+        /// <summary>
+        /// the verticies of the mesh
+        /// </summary>
+        public List<Vector3> verticies = new List<Vector3>(0);
+        /// <summary>
+        /// the indicies of the mesh
+        /// </summary>
+        public List<int> indicies = new List<int>(0);
+        int previousVertsLength = 0;
+        /// <summary>
+        /// add a piece of geometry to the mesh
+        /// </summary>
+        /// <param name="geometry">the geometry to add</param>
+        public void AddGeometry((Vector3[], int[]) geometry) =>
+            AddGeometry(geometry.Item1, geometry.Item2);
+        /// <summary>
+        /// add a piece of geometry to the mesh
+        /// </summary>
+        /// <param name="verticies">the verticies of the geometry</param>
+        /// <param name="indicies">the indicies of the geometry</param>
+        public void AddGeometry(Vector3[] verticies, int[] indicies)
+        {
+            this.verticies.AddRange(verticies);
+            this.indicies.AddRange(MathUtility.AddToList(indicies, previousVertsLength));
+            previousVertsLength = verticies.Length;
+        }
+        /// <summary>
+        /// clears the mesh data
+        /// </summary>
+        public void Clear()
+        {
+            verticies.Clear();
+            indicies.Clear();
+        }
+        /// <summary>
+        /// generates the mesh for use
+        /// </summary>
+        /// <returns>the generated mesh</returns>
+        public Mesh Generate()
+        {
+            Mesh Generated = new Mesh();
+            Generated.vertices = verticies.ToArray();
+            Generated.triangles = indicies.ToArray();
+            return Generated;
+        }
+    }
+}
