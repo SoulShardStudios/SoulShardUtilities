8d32acbf120913dbb1df904264e03be17e3d7225
Benjamin Hines <75751722+Lubba-64@users.noreply.github.com>
2022-07-27 20:08:42 -0400

    feat: Add many more features to the entity spawner

--- Runtime/Mono/EntitySpawner/Editor/EntitySpawner2DEditor.cs
+++ Runtime/Mono/EntitySpawner/Editor/EntitySpawner2DEditor.cs
@@ -4,11 +4,34 @@ using UnityEngine;
 [CustomEditor(typeof(EntitySpawner2D))]
 public class EntitySpawner2DEditor : Editor
 {
+    Vector2? prevpos = null;
+
+    public void OnSceneGUI()
+    {
+        var spawner = target as EntitySpawner2D;
+        var v3 = new Vector3[spawner.spawnArea.Length];
+        for (int i = 0; i < spawner.spawnArea.Length; i++)
+            v3[i] = (Vector3)spawner.spawnArea[i];
+        Handles.color = new Color(1, 1, 1, 0.1f);
+        Handles.DrawAAConvexPolygon(v3);
+        for (int i = 0; i < spawner.spawnArea.Length; i++)
+            spawner.spawnArea[i] = Handles.PositionHandle(
+                spawner.spawnArea[i]
+                    + (
+                        prevpos.HasValue
+                            ? ((Vector2)spawner.transform.position - prevpos.Value)
+                            : Vector2.zero
+                    ),
+                Quaternion.identity
+            );
+        prevpos = spawner.transform.position;
+    }
+
     public override void OnInspectorGUI()
     {
-        var spawner = (EntitySpawner2D)target;
+        var spawner = target as EntitySpawner2D;
         if (GUILayout.Button("Spawn Entities"))
-            spawner.SpawnEntities();
+            spawner.SpawnAllProps();
         base.OnInspectorGUI();
     }
 }
--- Runtime/Mono/EntitySpawner/EntitySpawner2D.cs
+++ Runtime/Mono/EntitySpawner/EntitySpawner2D.cs
@@ -1,67 +1,149 @@
 using UnityEngine;
 using UnityEditor;
-using SoulShard.Utils;
+
 /// <summary>
 /// Fast automation for randomly spawning entities over an area in a natural way.
 /// </summary>
 public class EntitySpawner2D : MonoBehaviour
 {
     /// <summary>
+    /// The properties for each stage of spawning.
+    /// </summary>
+    [System.Serializable]
+    public class Props
+    {
+        /// <summary>
+        /// The collection of entities that could be spawned (Selected randomly)
+        /// </summary>
+        public GameObject[] entities;
+
+        /// <summary>
+        /// The number of attempts to spawn an entity that should be performed.
+        /// </summary>
+        public Vector2 spawnAmount;
+
+        /// <summary>
+        /// The minimum distance away from other entities inside of the transform parent that new entities must be.
+        /// </summary>
+        public float minimumDistance;
+    }
+
+    /// <summary>
+    /// The list of properties of each stage of spawning.
+    /// Goes from the first element to the last and spawns using those parameters in order.
+    /// </summary>
+    [SerializeField]
+    Props[] _props;
+
+    /// <summary>
     /// The transform parent to spawn the entites to.
     /// </summary>
     [SerializeField]
     Transform _spawnParent;
+
     /// <summary>
-    /// The collection of entities that could be spawned (Selected randomly)
+    /// The polygon in which to spawn the entities
+    /// </summary>
+    public Vector2[] spawnArea;
+
+    /// <summary>
+    /// Should the script spawn entities on enable?
     /// </summary>
     [SerializeField]
-    GameObject[] _entities;
-    /// <summary>
-    /// The size of the area to spawn entities in.
-    /// </summary>
-    [SerializeField]
-    Vector2 _spawnSize;
-    /// <summary>
-    /// The number of attempts to spawn an entity that should be performed.
-    /// </summary>
-    [SerializeField]
-    Vector2 _spawnAmount;
-    /// <summary>
-    /// The minimum distance away from other entities inside of the transform parent that new entities must be.
-    /// </summary>
-    [SerializeField]
-    float _minimumDistance;
+    bool _spawnOnEnable;
+
+    void OnEnable()
+    {
+        if (_spawnOnEnable)
+            SpawnAllProps();
+    }
+
+    bool PointIsValid(Vector2 point, float minDist)
+    {
+        for (int t = 0; t < _spawnParent.childCount; t++)
+            if (Vector2.Distance(_spawnParent.GetChild(t).position, point) < minDist)
+                return false;
+        return true;
+    }
+
     /// <summary>
     /// With the parameters on this monobehavior, spawn some entities.
     /// </summary>
-    public void SpawnEntities()
+    public void SpawnAllProps()
     {
-        if (_entities.Length == 0)
+        foreach (Props p in _props)
+            SpawnEntities(p);
+    }
+
+    void SpawnEntities(Props props)
+    {
+        if (props.entities.Length == 0)
         {
             Debug.LogError("No entities were provided to the spawner.");
             return;
         }
-        for (int i = 0; i < Mathf.Ceil(Random.Range(_spawnAmount.x, _spawnAmount.y)); i++)
+        Mesh mesh = ConvexPointsToMesh2D(spawnArea, 0);
+        for (int i = 0; i < Mathf.Ceil(Random.Range(props.spawnAmount.x, props.spawnAmount.y)); i++)
         {
-            var pos = _randomPos;
-            bool distanced = true;
-            for (int t = 0; t < _spawnParent.childCount; t++)
-            {
-                if (Vector2.Distance(_spawnParent.GetChild(t).position, pos) < _minimumDistance)
-                {
-                    distanced = false;
-                    break;
-                }
-            }
-            if (!distanced)
+            var pos = GetRandomPointOnMesh(mesh);
+            if (!PointIsValid(pos, props.minimumDistance))
                 continue;
-            GameObject entity = _entities[Random.Range(0, _entities.Length)];
-            GameObject g = (GameObject)PrefabUtility.InstantiatePrefab(entity, _spawnParent.transform);
+            GameObject entity = props.entities[Random.Range(0, props.entities.Length)];
+            GameObject g = (GameObject)PrefabUtility.InstantiatePrefab(
+                entity,
+                _spawnParent.transform
+            );
             if (g == null)
                 g = Instantiate(entity, _spawnParent.transform);
             g.transform.position = pos;
         }
     }
-    void OnDrawGizmos() => GizmosUtility.DrawRect(new Rect(transform.position, _spawnSize));
-    Vector2 _randomPos => transform.position + new Vector3(Random.Range(0, _spawnSize.x), Random.Range(0, _spawnSize.y));
+
+    public static Mesh ConvexPointsToMesh2D(Vector2[] points, float z)
+    {
+        var mesh = new Mesh();
+        var pts = new Vector3[points.Length];
+        for (int i = 0; i < points.Length; i++)
+            pts[i] = new Vector3(points[i].x, points[i].y, z);
+
+        mesh.vertices = pts;
+        var triCount = points.Length - 2;
+        int[] tris = new int[triCount * 3];
+        for (int i = 0; i < triCount; i++)
+        {
+            tris[i * 3] = 0;
+            tris[i * 3 + 1] = i + 1;
+            tris[i * 3 + 2] = i + 2;
+        }
+
+        mesh.triangles = tris;
+        return mesh;
+    }
+
+    public static Vector3 GetRandomPointOnTriangle(Vector3 a, Vector3 b, Vector3 c)
+    {
+        float r = Random.value;
+        float s = Random.value;
+
+        if (r + s >= 1)
+        {
+            r = 1 - r;
+            s = 1 - s;
+        }
+
+        Vector3 pointOnMesh = a + r * (b - a) + s * (c - a);
+        return pointOnMesh;
+    }
+
+    public static Vector3 GetRandomPointOnMesh(Mesh mesh)
+    {
+        int tris = (mesh.triangles.Length / 3) - 1;
+        int triIndex = Mathf.RoundToInt(tris * Random.value);
+
+        Vector3 a = mesh.vertices[mesh.triangles[triIndex * 3]];
+        Vector3 b = mesh.vertices[mesh.triangles[triIndex * 3 + 1]];
+        Vector3 c = mesh.vertices[mesh.triangles[triIndex * 3 + 2]];
+
+        return GetRandomPointOnTriangle(a, b, c);
+    }
 }
