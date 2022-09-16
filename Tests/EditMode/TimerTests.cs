2576d2db5fa3934ea1262d638eb0b82cddfe44cc
Benjamin Hines <75751722+Lubba-64@users.noreply.github.com>
2022-09-15 21:52:12 -0400

    feat: Allow for finite and infinite retriggering

--- Tests/EditMode/TimerTests.cs
+++ Tests/EditMode/TimerTests.cs
@@ -1,6 +1,6 @@
 using SoulShard.Utils;
 using NUnit.Framework;
-
+using UnityEngine;
 public class TimerTests
 {
     [Test]
@@ -28,6 +28,17 @@ public class TimerTests
     }
 
     [Test]
+    public void TestOnCycleComplete()
+    {
+        bool done = false;
+        var timer = new Timer(0.5f);
+        timer.onCycleComplete += () => done = true;
+        for (int i = 0; i < 55; i++)
+            timer.Tick(0.05f);
+        Assert.True(done);
+    }
+
+    [Test]
     public void TestOnDone()
     {
         bool done = false;
@@ -39,14 +50,41 @@ public class TimerTests
     }
 
     [Test]
-    public void TestAutoReset()
+    public void TestAutoResetInfinite()
     {
-        int calls = 0;
-        var timer = new Timer(0.5f);
-        timer.autoReset = true;
-        timer.onDone += () => calls++;
+        int cycles = 0;
+        int dones = 0;
+        var timer = new Timer(0.5f, 0);
+        timer.onDone += () => dones++;
+        timer.onCycleComplete += () => cycles++;
         for (int i = 0; i < 10000; i++)
             timer.Tick(0.05f);
-        Assert.True(calls == 1000);
+        Assert.True(cycles == 1000);
+        Assert.True(dones == 0);
+        Assert.True(!timer.done);
+    }
+    [Test]
+    public void TestAutoResetFinite()
+    {
+        int cycles = 0;
+        int dones = 0;
+        var timer = new Timer(0.5f, 500);
+        timer.onDone += () => dones++;
+        timer.onCycleComplete += () => cycles++;
+        for (int i = 0; i < 10000; i++)
+            timer.Tick(0.05f);
+        Assert.True(cycles == 500);
+        Assert.True(dones == 1);
+    }
+    [Test]
+    public void TestCompleteCycle()
+    {
+        var timer = new Timer(10, 1);
+        timer.CompleteCycle();
+        Assert.True(timer.done);
+        var timer2 = new Timer(10, 2);
+        timer2.CompleteCycle();
+        timer2.CompleteCycle();
+        Assert.True(timer2.done);
     }
 }
