bde27d662b6f840fa5e0840c4bfc6e4c13d4bc7d
Benjamin Hines <75751722+Lubba-64@users.noreply.github.com>
2022-09-25 13:47:42 -0400

    fix: Fix bug where entitySpawner2D would not save its changes under some circumstances

--- Runtime/Mono/EntitySpawner/Editor/EntitySpawner2DEditor.cs
+++ Runtime/Mono/EntitySpawner/Editor/EntitySpawner2DEditor.cs
@@ -1,32 +1,39 @@
 using UnityEditor;
 using UnityEngine;
-
 namespace SoulShard.Utils
 {
-    [CustomEditor(typeof(EntitySpawner2D))]
+    [CustomEditor(typeof(EntitySpawner2D)), CanEditMultipleObjects]
     public class EntitySpawner2DEditor : Editor
     {
         public void OnSceneGUI()
         {
-            var spawner = target as EntitySpawner2D;
+            EditorGUI.BeginChangeCheck();
+            var spawner = (EntitySpawner2D)target;
+            var len = spawner.spawnArea.Length;
             if (spawner.spawnArea == null)
                 return;
 
-            var v3 = new Vector3[spawner.spawnArea.Length];
-            for (int i = 0; i < spawner.spawnArea.Length; i++)
+            var v3 = new Vector3[len];
+            for (int i = 0; i < len; i++)
                 v3[i] = (Vector3)spawner.spawnArea[i];
             Handles.color = new Color(1, 1, 1, 0.1f);
             Handles.DrawAAConvexPolygon(v3);
-            for (int i = 0; i < spawner.spawnArea.Length; i++)
-                spawner.spawnArea[i] = Handles.PositionHandle(
-                    spawner.spawnArea[i],
-                    Quaternion.identity
-                );
+            var v2 = new Vector2[len];
+            for (int i = 0; i < len; i++)
+            {
+                v2[i] = Handles.PositionHandle(
+                        spawner.spawnArea[i],
+                        Quaternion.identity
+                    );
+            }
+            EditorGUI.EndChangeCheck();
+            Undo.RecordObject(spawner, "edit spawnArea positions");
+            spawner.spawnArea = v2;
         }
 
         public override void OnInspectorGUI()
         {
-            var spawner = target as EntitySpawner2D;
+            var spawner = (EntitySpawner2D)target;
             if (GUILayout.Button("Spawn Entities"))
                 spawner.SpawnAllProps();
             base.OnInspectorGUI();
