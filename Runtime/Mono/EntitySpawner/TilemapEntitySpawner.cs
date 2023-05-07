67c2db886e2e1d53b5eb2a32e16fbe84c3f483a2
Benjamin Hines <75751722+Lubba-64@users.noreply.github.com>
2023-05-07 14:26:36 -0400

    feat: Allow avoiding of tilemaps in TilemapEntitySpawner

--- Runtime/Mono/EntitySpawner/TilemapEntitySpawner.cs
+++ Runtime/Mono/EntitySpawner/TilemapEntitySpawner.cs
@@ -8,19 +8,29 @@ namespace SoulShard.Utils
 {
     public class TilemapEntitySpawner : ExtendedMono
     {
+        [Tooltip(tooltip: "The tilemap to spawn entities on")]
         [SerializeField]
         Tilemap _tilemap;
 
+        [Tooltip(tooltip: "The tilemap to avoid placing entities on")]
+        [SerializeField]
+        Tilemap[] _forbiddenTilemaps;
+
+        [Tooltip(tooltip: "Whether the entities should spawn when the game starts")]
         [SerializeField]
         bool _spawnOnAwake;
 
+        [Tooltip(tooltip: "The spawn configurations to spawn")]
         [SerializeField]
         TilemapEntitySpawnerProps[] _props;
 
+        [Tooltip(tooltip: "The transform parent of the spawned entities")]
         [SerializeField]
         Transform _spawnParent;
 
         Dictionary<uint, Vector2Int[]> _spawnDeltaCache = new Dictionary<uint, Vector2Int[]>();
+
+        Vector3Int _tilemapMin => _tilemap.cellBounds.min;
 
         Vector2Int[] GetTileGapDeltas(uint size)
         {
@@ -42,6 +52,69 @@ namespace SoulShard.Utils
             return _spawnDeltaCache[size] = deltas.ToArray();
         }
 
+        bool OverlapsForbiddenTilemap(uint distance, Vector3Int tilePos)
+        {
+            foreach (var v in GetTileGapDeltas(distance))
+            {
+                var pos = v + (Vector2Int)tilePos;
+                foreach (var t in _forbiddenTilemaps)
+                    if (t.GetTile((Vector3Int)pos) != null)
+                        return true;
+            }
+            return false;
+        }
+
+        void ApplySpawnConfigToTile(
+            Rect rect,
+            TileBase tile,
+            Vector3Int tilePos,
+            TilemapEntitySpawnerProps[,] chosenConfigs
+        )
+        {
+            Vector2Int chosenConfigsIdx = (Vector2Int)(tilePos - _tilemapMin);
+            TilemapEntitySpawnerProps config = null;
+            foreach (var c in _props)
+            {
+                if (c.chance <= Random.Range(0f, 1.1f))
+                    continue;
+                if (!c.validTiles.Contains(tile))
+                    continue;
+                if (c.tileGap == 0)
+                {
+                    config = c;
+                    break;
+                }
+                foreach (var v in GetTileGapDeltas(c.tileGap))
+                {
+                    var pos = v + chosenConfigsIdx;
+                    TilemapEntitySpawnerProps configAtPos;
+                    try
+                    {
+                        configAtPos = chosenConfigs[pos.x, pos.y];
+                    }
+                    catch
+                    {
+                        continue;
+                    }
+                    if (configAtPos == c)
+                        return;
+                }
+                if (OverlapsForbiddenTilemap(config.tileGapToForbiddenTilemap, tilePos))
+                    return;
+                config = c;
+            }
+            if (config == null)
+                return;
+
+            chosenConfigs[chosenConfigsIdx.x, chosenConfigsIdx.y] = config;
+
+            for (int i = 0; i < config.spawnPerTile.RandomBetween(); i++)
+                InstantiateWithReference(
+                    config.entities.RandomElement(),
+                    _spawnParent
+                ).transform.position = rect.RandomInside();
+        }
+
         [ContextMenu("Spawn Entities")]
         void Spawn()
         {
@@ -50,52 +123,6 @@ namespace SoulShard.Utils
                 _tilemap.cellBounds.size.y
             ];
 
-            Vector3Int tilemapMin = _tilemap.cellBounds.min;
-
-            void ApplySpawnConfigToTile(Rect rect, TileBase tile, Vector3Int tilePos)
-            {
-                Vector2Int chosenConfigsIdx = (Vector2Int)(tilePos - tilemapMin);
-                TilemapEntitySpawnerProps config = null;
-                foreach (var c in _props)
-                {
-                    if (c.chance <= Random.Range(0f, 1.1f))
-                        continue;
-                    if (!c.validTiles.Contains(tile))
-                        continue;
-                    if (c.tileGap == 0)
-                    {
-                        config = c;
-                        break;
-                    }
-                    foreach (var v in GetTileGapDeltas(c.tileGap))
-                    {
-                        var pos = v + chosenConfigsIdx;
-                        TilemapEntitySpawnerProps configAtPos;
-                        try
-                        {
-                            configAtPos = chosenConfigs[pos.x, pos.y];
-                        }
-                        catch
-                        {
-                            continue;
-                        }
-                        if (configAtPos == c)
-                            return;
-                    }
-                    config = c;
-                }
-                if (config == null)
-                    return;
-
-                chosenConfigs[chosenConfigsIdx.x, chosenConfigsIdx.y] = config;
-
-                for (int i = 0; i < config.spawnPerTile.RandomBetween(); i++)
-                    InstantiateWithReference(
-                        config.entities.RandomElement(),
-                        _spawnParent
-                    ).transform.position = rect.RandomInside();
-            }
-
             var grid = _tilemap.layoutGrid;
             for (var x = _tilemap.cellBounds.min.x; x < _tilemap.cellBounds.max.x; x++)
                 for (var y = _tilemap.cellBounds.min.y; y < _tilemap.cellBounds.max.y; y++)
@@ -103,7 +130,7 @@ namespace SoulShard.Utils
                     var tilePos = new Vector3Int(x, y, 0);
                     var tile = _tilemap.GetTile(tilePos);
                     var worldRect = new Rect(grid.CellToWorld(tilePos), grid.cellSize);
-                    ApplySpawnConfigToTile(worldRect, tile, tilePos);
+                    ApplySpawnConfigToTile(worldRect, tile, tilePos, chosenConfigs);
                 }
         }
 
--- Runtime/Mono/EntitySpawner/TilemapEntitySpawnerProps.cs
+++ Runtime/Mono/EntitySpawner/TilemapEntitySpawnerProps.cs
@@ -6,14 +6,26 @@ namespace SoulShard.Utils
     [CreateAssetMenu(menuName = "SoulShardUtils/TilemapEntitySpawnerProperties")]
     public class TilemapEntitySpawnerProps : ScriptableObject
     {
+        [Tooltip(tooltip: "The entities that can be spawned on this tile")]
         public GameObject[] entities;
+
+        [Tooltip(tooltip: "The chance that this spawn config will be used")]
         public float chance;
+
+        [Tooltip(tooltip: "How many things can be spawned on this tile")]
         public Vector2Int spawnPerTile;
+
+        [Tooltip(tooltip: "Which tiles are valid to spawn this entity on")]
         public TileBase[] validTiles;
 
         [Tooltip(
             tooltip: "The minimum distance in tiles between spawned members of the same configuration."
         )]
         public uint tileGap;
+
+        [Tooltip(
+            tooltip: "The minimum gap distance between where this spawn config will be applied and a forbidden tilemap"
+        )]
+        public uint tileGapToForbiddenTilemap;
     }
 }
